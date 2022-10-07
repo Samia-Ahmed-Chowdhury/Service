c298fd73d88f41623ce6e0a5fe38142813acee84
Samia-Ahmed-Chowdhury <106830587+Samia-Ahmed-Chowdhury@users.noreply.github.com>
2022-10-07 19:27:13 +0600

    Update Product.js

--- Product.js
+++ Product.js
@@ -2,11 +2,11 @@
     {
       "id": 1,
       "title": "Samsung S21",
-      "des": "black in color",
+      "des": "black in color"
     },
      {
       "id": 2,
       "title": "Sam",
-      "des": "black",
+      "des": "black"
     }
-  ];
+  ]
